0bb8ca8f9f4e78b126557f5b29a016939a6bd408
Akshada Shelke <84895276+Akshada13@users.noreply.github.com>
2021-11-30 11:31:44 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -5,8 +5,7 @@ let getSetting = () => {
         musicapi: "https://jiosaavn-api-v3.vercel.app/search?query=",
         finalmusicapi: "https://jiosaavn-api-v3.vercel.app/song?id=",
         github: "https://github.com/Akshada13/Music-Player-Frontend"
-        
-    }
+       }
 }
 export default getSetting()
 
